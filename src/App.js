09aacd64e25d147c4d9ab83334842174977ac85f
thomasliu244 <45839764+thomasliu244@users.noreply.github.com>
2020-06-09 22:20:56 -0400

    changed front page text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Thomas Liu's Personal Site
         </p>
         <a
           className="App-link"
