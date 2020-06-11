ab490c11b22d1b485dbed63a4e985820f2c166e5
thomasliu244 <45839764+thomasliu244@users.noreply.github.com>
2020-06-10 20:13:34 -0400

    new links and formatted in bootstrap

--- src/App.js
+++ src/App.js
@@ -1,41 +1,98 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class Title extends React.Component{
+  render(){
+    return(
+      <p className="Description">
+      About Thomas Liu
+    </p>
+    );
+  }
+}
+
+class ButtonLink extends React.Component{
+  render(){
+    return (
+      <a
+        className="App-link"
+        href={this.props.target}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img className="Social-media" src={this.props.imageSource}></img>
+      </a>     
+    );
+  }
+}
+
+function Table(){
   return (
-    <div className="App">
-      <section className="App-body">
-        <p className="Description">
-          Learn About Thomas Liu
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/thomasliu244"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img className="Social-media" src="images/GitHub-Mark-Light-64px.png"></img>
-        </a>
-        <a
-          className="App-link"
-          href="https://www.linkedin.com/in/thomas-liu-29a74b42/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img className="Social-media" src="images/linkedin.png"></img>
-        </a>
-        <a
-          className="App-link"
-          href="https://steamcommunity.com/id/tomtomknifez/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img className="Social-media" src="images/steam.png"></img>
-        </a>
-      </section>
+    <div id="main-table"  class="container text-center container-width-override">
+      <div class="row">
+        <div class="col"> 
+          <p>Gaming</p>
+        </div>
+        <div class="col"> 
+          <p>Work</p>
+        </div>
+        <div class="col"> 
+          <p>Social</p>
+        </div>
+      </div>
+      <div class="row" >
+        <div class="col">
+          <ButtonLink 
+            target="https://steamcommunity.com/id/tomtomknifez/"
+            imageSource="images/steam.png">
+          </ButtonLink>
+        </div>
+        <div class="col">
+          <ButtonLink 
+            target="https://www.twitch.tv/daknifez/"
+            imageSource="images/twitch.png">
+          </ButtonLink>
+        </div>
+        <div class="col">
+          <ButtonLink 
+            target="https://www.linkedin.com/in/thomas-liu-29a74b42/"
+            imageSource="images/linkedin.png">
+          </ButtonLink>
+        </div>
+        <div class="col">
+          <ButtonLink 
+            target="https://github.com/thomasliu244"
+            imageSource="images/GitHub-Mark-Light-64px.png">
+          </ButtonLink>
+        </div>
+        <div class="col">
+          <ButtonLink 
+            target="https://twitter.com/thomasliu"
+            imageSource="images/twitter.png">
+          </ButtonLink>
+        </div>
+        <div class="col">
+          <ButtonLink 
+            target="https://www.quora.com/profile/Thomas-Liu-126"
+            imageSource="images/quora.png">
+          </ButtonLink>
+        </div>
+      </div>
     </div>
   );
+}
+
+class App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+        <section className="App-body">
+          <Title />
+          <Table />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
