ad87952fde5433e8b7bb609ab7f4705ec6a82a8f
thomasliu244 <45839764+thomasliu244@users.noreply.github.com>
2020-06-11 11:32:59 -0400

    added some conditionals to make code cleaner

--- src/App.js
+++ src/App.js
@@ -16,14 +16,16 @@ class Footer extends React.Component{
 class ButtonLink extends React.Component{
   render(){
     return (
-      <a
-        className="App-link"
-        href={this.props.target}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img className="Social-media" src={this.props.imageSource}></img>
-      </a>     
+      <div class={this.props.isLeft ? "col left" : "col right"}>  
+        <a
+          className="App-link"
+          href={this.props.target}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img className={"Social-media "} src={this.props.imageSource}></img>
+        </a>
+      </div>     
     );
   }
 }
@@ -46,54 +48,46 @@ function Table(){
         </div>
       </div>
       <div class="row" >
-        <div class="col left">
-          <ButtonLink 
-            target="https://www.linkedin.com/in/thomas-liu-29a74b42/"
-            imageSource="images/linkedin.png">
-          </ButtonLink>
-        </div>
-        <div class="col right">
-          <ButtonLink 
-            target="https://github.com/thomasliu244"
-            imageSource="images/GitHub-Mark-Light-64px.png">
-          </ButtonLink>
-        </div>
-        <div class="col left">
-          <ButtonLink 
-            target="https://twitter.com/thomasliu"
-            imageSource="images/twitter.png">
-          </ButtonLink>
-        </div>
-        <div class="col right">
-          <ButtonLink 
-            target="https://www.quora.com/profile/Thomas-Liu-126"
-            imageSource="images/quora.png">
-          </ButtonLink>
-        </div>
-        <div class="col left">
-          <ButtonLink 
-            target="https://steamcommunity.com/id/tomtomknifez/"
-            imageSource="images/steam.png">
-          </ButtonLink>
-        </div>
-        <div class="col right">
-          <ButtonLink 
-            target="https://www.twitch.tv/daknifez/"
-            imageSource="images/twitch.png">
-          </ButtonLink>
-        </div>
-        <div class="col left">
-          <ButtonLink 
-            target="https://www.alltrails.com/members/thomas-liu-2"
-            imageSource="images/alltrails.png">
-          </ButtonLink>
-        </div>
-        <div class="col right">
-          <ButtonLink 
-            target="https://www.strava.com/athletes/thomas-liu"
-            imageSource="images/strava.png">
-          </ButtonLink>
-        </div>
+        <ButtonLink
+          isLeft={true} 
+          target="https://www.linkedin.com/in/thomas-liu-29a74b42/"
+          imageSource="images/linkedin.png">
+        </ButtonLink>
+        <ButtonLink
+          isLeft={false}
+          target="https://github.com/thomasliu244"
+          imageSource="images/GitHub-Mark-Light-64px.png">
+        </ButtonLink>
+        <ButtonLink
+          isLeft={true}  
+          target="https://twitter.com/thomasliu"
+          imageSource="images/twitter.png">
+        </ButtonLink>
+        <ButtonLink 
+          isLeft={false}
+          target="https://www.quora.com/profile/Thomas-Liu-126"
+          imageSource="images/quora.png">
+        </ButtonLink>
+        <ButtonLink 
+          isLeft={true} 
+          target="https://steamcommunity.com/id/tomtomknifez/"
+          imageSource="images/steam.png">
+        </ButtonLink>
+        <ButtonLink 
+          isLeft={false}
+          target="https://www.twitch.tv/daknifez/"
+          imageSource="images/twitch.png">
+        </ButtonLink>
+        <ButtonLink 
+          isLeft={true} 
+          target="https://www.alltrails.com/members/thomas-liu-2"
+          imageSource="images/alltrails.png">
+        </ButtonLink>
+        <ButtonLink 
+          isLeft={false}
+          target="https://www.strava.com/athletes/thomas-liu"
+          imageSource="images/strava.png">
+        </ButtonLink>
       </div>
     </div>
   );
