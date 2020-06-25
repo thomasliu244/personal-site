0f7ca9f5790ba28200988df5f0409f6b9d013146
thomasliu244 <45839764+thomasliu244@users.noreply.github.com>
2020-06-24 20:20:41 -0400

    Updated site to include text

--- src/App.js
+++ src/App.js
@@ -1,16 +1,84 @@
 import React, { useState } from 'react';
 import './App.css';
 
-class Footer extends React.Component{
-  render(){
-    return(
-      <footer class="footer">
-        <div class="container">
-          <span class="text-muted">Created by Thomas Liu</span>
+function Header(){
+  return(
+    <div class="container text-center container-default-override">
+      <div class="row">
+        <div class="col" >
+          <h1 id="page-header">Thomas Liu's Personal Site</h1>
+          <p id="page-body">Hi there! Welcome to my website. 
+          <br></br><br></br>Below you will find a collection of my projects and hobbies.
+          </p>
         </div>
-      </footer>
-    );
-  }
+      </div>
+    </div>
+  )
+}
+
+function SocialMedia(){
+  return (
+    <div class="container text-center container-default-override main-table">
+      <div class="row icon-cats">
+        <div class="col-3" > 
+          <p>Work</p>
+        </div>
+        <div class="col-3"> 
+          <p>Gaming</p>
+        </div>
+        <div class="col-3"> 
+          <p>Outdoors</p>
+        </div>
+        <div class="col-3"> 
+          <p>Game Dev</p>
+        </div>
+      </div>
+      <div class="row" >
+        <ButtonLink
+          position="col left"
+          target="https://www.linkedin.com/in/thomas-liu-29a74b42/"
+          imageSource="images/linkedin.png"
+          label="LinkedIn">
+        </ButtonLink>
+        <ButtonLink
+          position="col right"
+          target="https://github.com/thomasliu244"
+          imageSource="images/GitHub-Mark-Light-64px.png"
+          label="GitHub">
+        </ButtonLink>
+        <ButtonLink 
+          position="col left"
+          target="https://steamcommunity.com/id/tomtomknifez/"
+          imageSource="images/steam.png"
+          label="Steam">
+        </ButtonLink>
+        <ButtonLink 
+          position="col right"
+          target="https://www.twitch.tv/daknifez/"
+          imageSource="images/twitch.png"
+          label="Twitch">
+        </ButtonLink>
+        <ButtonLink 
+          position="col left"
+          target="https://www.alltrails.com/members/thomas-liu-2"
+          imageSource="images/alltrails.png"
+          label="AllTrails">
+        </ButtonLink>
+        <ButtonLink 
+          position="col right"
+          target="https://www.strava.com/athletes/thomas-liu"
+          imageSource="images/strava.png"
+          label="Strava">
+        </ButtonLink>
+        <ButtonLink 
+          position= "col-3"
+          target="unitytest/index.html"
+          imageSource="images/unity.png"
+          label="Unity Testing">
+        </ButtonLink>
+      </div>
+    </div>
+  );
 }
 
 class ButtonLink extends React.Component{
@@ -42,95 +110,25 @@ class ButtonLink extends React.Component{
   }
 }
 
-function Table(){
-  return (
-    <div class="container text-center container-default-override main-table">
-      <div class="row icon-cats">
-        <div class="col" > 
-          <p>Work</p>
-        </div>
-        <div class="col"> 
-          <p>Social</p>
-        </div>
-        <div class="col"> 
-          <p>Gaming</p>
-        </div>
-        <div class="col"> 
-          <p>Outdoors</p>
-        </div>
+function AboutMe(){
+  return(
+    <div class="container text-center container-default-override">
+      <div class="row">
+        <div class="col"><h1 id="aboutme-header">About Me</h1></div>
       </div>
-      <div class="row" >
-        <ButtonLink
-          position="col left"
-          target="https://www.linkedin.com/in/thomas-liu-29a74b42/"
-          imageSource="images/linkedin.png"
-          label="LinkedIn">
-        </ButtonLink>
-        <ButtonLink
-          position="col right"
-          target="https://github.com/thomasliu244"
-          imageSource="images/GitHub-Mark-Light-64px.png"
-          label="GitHub">
-        </ButtonLink>
-        <ButtonLink
-          position="col left" 
-          target="https://twitter.com/thomasliu"
-          imageSource="images/twitter.png"
-          label="Twitter">
-        </ButtonLink>
-        <ButtonLink 
-          position="col right"
-          target="https://www.quora.com/profile/Thomas-Liu-126"
-          imageSource="images/quora.png"
-          label="Quora">
-        </ButtonLink>
-        <ButtonLink 
-          position="col left"
-          target="https://steamcommunity.com/id/tomtomknifez/"
-          imageSource="images/steam.png"
-          label="Steam">
-        </ButtonLink>
-        <ButtonLink 
-          position="col right"
-          target="https://www.twitch.tv/daknifez/"
-          imageSource="images/twitch.png"
-          label="Twitch">
-        </ButtonLink>
-        <ButtonLink 
-          position="col left"
-          target="https://www.alltrails.com/members/thomas-liu-2"
-          imageSource="images/alltrails.png"
-          label="AllTrails">
-        </ButtonLink>
-        <ButtonLink 
-          position="col right"
-          target="https://www.strava.com/athletes/thomas-liu"
-          imageSource="images/strava.png"
-          label="Strava">
-        </ButtonLink>
+      <div class="row">
+        <div class="col-3" >
+          <img id="self-portrait" src="images/self-portrait.jpg"></img>
+        </div>
+        <div class="col" >
+          <p id="aboutme-body">          
+          I am a technology product manager and entrepreneur passionate about building products for humans.
+          <br></br><br></br>In my spare time, you will most likely find me on my bicycle exploring NYC or playing video games on my PC. You can occasionally catch me streaming on Twitch.          
+          </p>
+        </div>
       </div>
     </div>
-  );
-}
-
-function Table2(){
-  return (
-    <div class="main-table container text-center container-default-override">
-      <div class="row icon-cats">
-        <div class="col" > 
-          <p>Game Dev</p>
-        </div>
-      </div>
-      <div class="row" >
-        <ButtonLink 
-          position= "col"
-          target="unitytest/index.html"
-          imageSource="images/unity.png"
-          label="Playing around in Unity">
-        </ButtonLink>
-      </div>
-    </div>
-  );
+  )
 }
 
 class App extends React.Component {
@@ -138,11 +136,9 @@ class App extends React.Component {
     return (
       <div className="App">
         <section className="App-body">
-          <h4 id="firstHeader" class="pageHeaders">My Stuff Online</h4>
-          <Table />
-          <h4 class="pageHeaders">Random Projects</h4>
-          <Table2 />
-          <Footer />
+          <Header />
+          <SocialMedia />
+          <AboutMe />
         </section>
       </div>
     );
